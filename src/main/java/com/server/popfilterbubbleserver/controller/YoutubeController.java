b2e8f66d905c4866096e6b28028611e00e2b8736
imygnam <imygnam@gmail.com>
2023-05-22 18:50:56 +0900

    [FIX] mutex 추가

--- src/main/java/com/server/popfilterbubbleserver/controller/YoutubeController.java
+++ src/main/java/com/server/popfilterbubbleserver/controller/YoutubeController.java
@@ -14,13 +14,23 @@ import java.io.IOException;
 public class YoutubeController {
     private final YoutubeService youtubeService;
 
+    private boolean isProcessing = false;
+
     @GetMapping("/politics")
     public TotalDTO getPolitics(@RequestParam String[] channelId) throws IOException {
+        if(isProcessing)
+            return null;
+
+        isProcessing = true;
+        try{
         PoliticsDTO politicsDTO = youtubeService.getPoliticsDto(channelId);
         List<VideoListDTO> videoListDTOS = youtubeService.getVideoListDto(channelId);
         return TotalDTO.builder()
                 .politicsDTO(politicsDTO)
                 .videoListDTO(videoListDTOS)
                 .build();
+        } finally {
+            isProcessing = false;
+        }
     }
 }
