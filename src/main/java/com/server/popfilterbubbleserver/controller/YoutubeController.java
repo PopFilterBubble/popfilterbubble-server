1845114af56a50ab849e8db89ddc53dc138eaa63
imygnam <imygnam@gmail.com>
2023-06-03 01:21:05 +0900

    [FEAT] mutex 개선 및 성능 향상

--- src/main/java/com/server/popfilterbubbleserver/controller/YoutubeController.java
+++ src/main/java/com/server/popfilterbubbleserver/controller/YoutubeController.java
@@ -14,23 +14,16 @@ import java.io.IOException;
 public class YoutubeController {
     private final YoutubeService youtubeService;
 
-    private boolean isProcessing = false;
-
     @GetMapping("/politics")
     public TotalDTO getPolitics(@RequestParam String[] channelId) throws IOException {
-        if(isProcessing)
-            return null;
 
-        isProcessing = true;
-        try{
+
         PoliticsDTO politicsDTO = youtubeService.getPoliticsDto(channelId);
         List<VideoListDTO> videoListDTOS = youtubeService.getVideoListDto(channelId);
         return TotalDTO.builder()
                 .politicsDTO(politicsDTO)
                 .videoListDTO(videoListDTOS)
                 .build();
-        } finally {
-            isProcessing = false;
-        }
+
     }
 }
